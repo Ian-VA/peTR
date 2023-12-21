c132cc34c719f2a36c130e6474774bbeba4baa92
ian <minedragocraft@gmail.com>
2023-12-20 22:33:31 -0800

    train

--- efficiencytest/dataprep.py
+++ efficiencytest/dataprep.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from torch.utils.data import TensorDataset, DataLoader
+import torch
 
 df = pd.read_csv("electricdata.csv")
 df = df.set_index(['DATE'])
@@ -11,12 +15,77 @@ if not df.index.is_monotonic:
 
 df = df.rename(columns={'Date': 'Value'})
 
+df_features = (
+         df
+         .assign(month = df.index.month)
+         .assign(day = df.index.day)
+        )
+
+def generate_cyclical_features(df, col_name, period, start_num=0):
+    kwargs = {
+        f'sin_{col_name}' : lambda x: np.sin(2*np.pi*(df[col_name]-start_num)/period),
+        f'cos_{col_name}' : lambda x: np.cos(2*np.pi*(df[col_name]-start_num)/period)    
+             }
+    return df.assign(**kwargs).drop(columns=[col_name])
+
+def onehot(df, col):
+    dummy = pd.get_dummies(df[col], prefix=col)
+    return pd.concat([df, dummy], axis=1).drop(columns=[col])
+
 def plot(title):
     plt.plot(df.index, df.values)
     plt.title(title)
     plt.show()
 
-plot("Electricity Prices Over Time")
+def lag(df, n_lags):
+    df_n = df.copy()
+    for n in range(1, n_lags + 1):
+        df_n[f"lag{n}"] = df_n["value"].shift(n)
+    df_n = df_n.iloc[n_lags:]
+    return df_n
 
-    
+def feature_label_split(df, target_col):
+    y = df[[target_col]]
+    X = df.drop(columns=[target_col])
+    return X, y
+
+def train_val_test_split(df, target_col, test_ratio):
+    val_ratio = test_ratio / (1 - test_ratio)
+    X, y = feature_label_split(df, target_col)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_ratio, shuffle=False)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=val_ratio, shuffle=False)
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+def get_train():
+    x_train, _, _, _, _, _ = train_val_test_split(df_features, 'value', 0.2)
+    return x_train
+
+def get_data():
+    X_train, X_val, X_test, y_train, y_val, y_test = train_val_test_split(df_features, 'value', 0.2)
+    scaler = MinMaxScaler()
+    X_train_arr = scaler.fit_transform(X_train)
+    X_val_arr = scaler.transform(X_val)
+    X_test_arr = scaler.transform(X_test)
+
+    y_train_arr = scaler.fit_transform(y_train)
+    y_val_arr = scaler.transform(y_val)
+    y_test_arr = scaler.transform(y_test)
+    batch_size = 64
+
+    train_features = torch.Tensor(X_train_arr)
+    train_targets = torch.Tensor(y_train_arr)
+    val_features = torch.Tensor(X_val_arr)
+    val_targets = torch.Tensor(y_val_arr)
+    test_features = torch.Tensor(X_test_arr)
+    test_targets = torch.Tensor(y_test_arr)
+
+    train = TensorDataset(train_features, train_targets)
+    val = TensorDataset(val_features, val_targets)
+    test = TensorDataset(test_features, test_targets)
+
+    train_loader = DataLoader(train, batch_size=batch_size, shuffle=False, drop_last=True)
+    val_loader = DataLoader(val, batch_size=batch_size, shuffle=False, drop_last=True)
+    test_loader = DataLoader(test, batch_size=batch_size, shuffle=False, drop_last=True)
+
+    return train_loader, val_loader, test_loader
 
