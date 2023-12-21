f86762c711fd897e1f7d41361fe89ce83e29bdd0
ian <minedragocraft@gmail.com>
2023-12-20 21:46:37 -0800

    figs

--- efficiencytest/dataprep.py
+++ efficiencytest/dataprep.py
@@ -3,9 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("electricdata.csv")
+df = df.set_index(['DATE'])
+df.index = pd.to_datetime(df.index)
+
+if not df.index.is_monotonic:
+    df = df.sort_index()
+
+df = df.rename(columns={'Date': 'Value'})
 
 def plot(title):
-    plt.plot(df["DATE"], df["IPG2211A2N"])
+    plt.plot(df.index, df.values)
+    plt.title(title)
     plt.show()
 
 plot("Electricity Prices Over Time")
